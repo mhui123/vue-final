74fe07210ca00213177b661318d5d914485616d6
mhui123 <mhui7864@gmail.com>
2024-02-18 17:06:33 +0900

    실무용 환경설정

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+});
+console.log('url : ' + process.env.VUE_APP_API_URL);
 function registerUser(userData) {
-  const url = 'http://localhost:3000/signup';
-  return axios.post(url, userData);
+  return instance.post('signup', userData);
 }
 
 export { registerUser };
